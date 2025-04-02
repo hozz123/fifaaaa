46a0ce45adb90265a086d5e8b28aa4ad9df25e1e
hozz123 <yousefhozz21@gmail.com>
2025-04-02 00:04:29 -0400

    Update fifadashboard.py

--- fifadashboard.py
+++ fifadashboard.py
@@ -103,8 +103,11 @@ def show_year_results(year):
     return "Select a year to view the result."
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
+
 
 
 
